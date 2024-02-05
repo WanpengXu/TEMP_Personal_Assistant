8aae0cb5cd5af31d32ce607676861796b73c22d5
WanpengXu <xwp@cumt.edu.cn>
2024-02-05 19:25:44 +0800

    6th

--- start.py
+++ start.py
@@ -26,5 +26,6 @@ xtuner convert merge \
 # run
 os.system('pwd')
 os.system('ls')
-os.system('cd InternLM_Lite')
-os.system('streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860')
+# os.system('cd ~/InternLM_Lite')
+os.system('cd /home/xlab-app-center/InternLM_Lite')
+os.system('streamlit run ~/InternLM_Lite/web_demo.py --server.address=0.0.0.0 --server.port 7860')
