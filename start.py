53e8ad6571826ff8af448d2195685e96c4f01ad1
WanpengXu <xwp@cumt.edu.cn>
2024-02-05 17:59:43 +0800

    2nd

--- start.py
+++ start.py
@@ -7,16 +7,12 @@ os.system('git clone -b v0.1.9 https://gitee.com/Internlm/xtuner')
 os.system('cd xtuner')
 os.system("pip install -e '.[all]'")
 
-
 # OpenXLab 环境配置
-
 # 导入基座模型
 from openxlab.model import download
 download(model_repo='OpenLMLab/InternLM-chat-7b', output='/home/xlab-app-center/model/InternLM-chat-7b')
-
 # 导入 Adapter
 download(model_repo='WanpengXu/Personal_Assistant_Adapter', output='/home/xlab-app-center/model/pa_hf_weights')
-import os
 
 # Merge
 os.system('''
